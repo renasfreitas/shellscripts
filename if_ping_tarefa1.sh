7dea4090b43f0e46ce614b2ee834cfc3930ef700
renasfreitas <renasfreitas@gmail.com>
2019-05-27 12:57:40 -0300

    Update and rename tarefa1.sh to if_ping_tarefa1.sh

--- if_ping_tarefa1.sh
+++ if_ping_tarefa1.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+# Criado por: Renã Freitas da cruz.
+# Editado por:    Na data:
+# Data: 06/05/2019
+# Exercicio de aula para criar um teste de conectividade usando o "if"
+
 echo "Digite um IP para ser testado:"
 read ip
 ping $ip -c 1 >/dev/null;
