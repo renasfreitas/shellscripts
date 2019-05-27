3a910bfb84f89ac50dcfa846fcf93217917bc909
renasfreitas <renasfreitas@gmail.com>
2019-05-27 13:04:31 -0300

    Update and rename teste_servico2.sh to if_teste_exec_servico2.sh

--- if_teste_exec_servico2.sh
+++ if_teste_exec_servico2.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+# Criado por: Renã Freitas da cruz.
+# Editado por:    Na data:
+# Data: 20/05/2019
+# Exercicio de aula para CASO um serviço esteja no ar, reiniciar ele
+
 echo "Digite o nome do programa:"
 read nome
 teste=`ls /etc/init.d/$nome`;
@@ -11,7 +16,3 @@ else
 	echo "nao ha servico"
 
 fi
-
-
-#echo $teste
-
