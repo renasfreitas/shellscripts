3b6d9f54ab0c92434bae9783a7e0294a292631df
renasfreitas <renasfreitas@gmail.com>
2019-05-27 13:02:19 -0300

    Update teste_servico.sh

--- teste_servico.sh
+++ teste_servico.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
+# Criado por: Renã Freitas da cruz.
+# Editado por:    Na data:
+# Data: 20/05/2019
+# Exercicio de aula para testar se programa esta em execução
 echo "Digite o nome do programa:"
 read nome
 teste=`ps -aux | grep $nome | grep -v grep`;
-#echo $ps
-
 if [ "$teste" ];
 then 
 echo "funcionando"
 else
 echo "nao esta funcionando"
 fi 
-
