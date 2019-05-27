d6ba2d573072a434037aeb0e1600340f2b77bceb
renasfreitas <renasfreitas@gmail.com>
2019-05-27 13:06:01 -0300

    Update teste_servico3.sh

--- teste_servico3.sh
+++ teste_servico3.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
+# Criado por: Renã Freitas da cruz.
+# Editado por:    Na data:
+# Data: 20/05/2019
+# Exercicio de aula reiniciar servico se estiver em execucao ou instalar ele.
 echo "Digite o nome do programa:"
 read nome
 teste=`ls /etc/init.d/$nome`;
 
 if [ $teste ] 
 then
-$teste restart
+  $teste restart
 else
-apt-get install $teste
+  apt-get install $teste
 
 fi
